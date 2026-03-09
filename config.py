9fabd04e5f3d62ba2207a1ddec97149171a48423
KumsalTR 🇹🇷 <sikmegdgfdf@gmail.com>
2026-03-09 18:51:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheAloneTeam/AloneMusic",
+    "https://github.com/KumsalTR63/KumsalMeldi",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
