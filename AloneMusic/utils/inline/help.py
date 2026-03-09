4048639c2278dc82d2f34eae8dda0c45032f7c90
KumsalTR 🇹🇷 <sikmegdgfdf@gmail.com>
2026-03-09 19:40:31 +0300

    Update help.py

--- AloneMusic/utils/inline/help.py
+++ AloneMusic/utils/inline/help.py
@@ -5,64 +5,56 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from AloneMusic import app
 import config
 
+def help_pannel(, START: Union[bool, int] = None):
+first = [InlineKeyboardButton(text=["CLOSE_BUTTON"], callback_data=f"close")]
+second = [
+InlineKeyboardButton(
+text=["BACK_BUTTON"],
+callback_data=f"settingsback_helper",
+),
+]
+mark = second if START else first
+upl = InlineKeyboardMarkup(
+[
+[
+InlineKeyboardButton(
+text=["S_B_3"],
+url=f"https://t.me/{app.username}?startgroup=true",
+)
+],
+[
+InlineKeyboardButton(
+text=["H_B_1"],
+callback_data="help_callback hb1",
+),
+InlineKeyboardButton(
+text=["H_B_2"],
+callback_data="help_callback hb2",
+),
+],
+[
+InlineKeyboardButton(text=["S_B_2"], url=config.SUPPORT_CHAT),
+InlineKeyboardButton(text=["S_B_5"], user_id=config.OWNER_ID),
+],
+[
+InlineKeyboardButton(text="✚  ᴋᴀʏɴᴀᴋ ᴋᴏᴅᴜ  ", url="https://www.youtube.com/@EpikTv87"),
+],
+mark,
+]
+)
+return upl
 
-def help_pannel(_, START: Union[bool, int] = None):
-    upl = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_3"],
-                    url=f"https://t.me/{app.username}?startgroup=true",
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-            ],
-            [
-                InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-                InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="✚ ᴋᴀʏɴᴀᴋ ᴋᴏᴅᴜ ✚",
-                    url="https://github.com/KumsalTR63/KumsalMeldi/blob/master/AloneMusic"
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"],
-                    callback_data="close"
-                )
-            ],
-        ]
-    )
-    return upl
-
-
-def private_help_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_4"],
-                url=f"https://t.me/{app.username}?start=help",
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                user_id=config.OWNER_ID
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="✚ ᴋᴀʏɴᴀᴋ ᴋᴏᴅᴜ ✚",
-                url="https://github.com/KumsalTR63/KumsalMeldi/blob/master/AloneMusic"
-            ),
-        ],
-    ]
-    return buttons
+def private_help_panel():
+buttons = [
+[
+InlineKeyboardButton(
+text=["S_B_4"],
+url=f"https://t.me/{app.username}?start=help",
+),
+InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+],
+[
+InlineKeyboardButton(text="✚ ᴋᴀʏɴᴀᴋ ᴋᴏᴅᴜ ✚", url="https://www.youtube.com/@EpikTv87"),
+],
+]
+return buttons
