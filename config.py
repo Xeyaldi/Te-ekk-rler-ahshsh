09d8b4f6a94173651e92bea3696c952bb31642ee
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-03-16 18:37:22 +0400

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/Nunum_tag_bot",
+    "https://t.me/Ht_userbot_qurulum_bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
